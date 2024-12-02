e7618d573f702d800828641346918f4cb1644893
NIKIEMAJ <ajulesnikiema@gmail.com>
2024-12-02 23:29:11 +0000

    first commit

---
+++ components/PokemonCard.js
@@ -0,0 +1,15 @@
+import {View, Text} from "react-native";
+
+
+export default function PokemonCard () {
+    
+    return (
+        <View>
+            <Text>
+                Pokememon card
+            </Text>
+        </View>
+    )
+  
+
+}
